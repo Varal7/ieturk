2e686a607bd8770501064b9f0adc34d8dfb998d4
Victor Quach <varaal@gmail.com>
2018-12-17 11:54:29 -0500

    Add tokenizer

---
+++ deal.py
@@ -0,0 +1,13 @@
+import csv
+import spacy
+
+
+nlp = spacy.load('en')
+
+csvin = csv.reader(open("raw.csv"))
+csvout = csv.writer(open("input.csv", 'w'))
+next(csvin)
+for row in csvin:
+    doc = nlp(row[1])
+    out_row = [ row[0], ">>".join([t.text for t in doc])]
+    csvout.writerow(out_row)
